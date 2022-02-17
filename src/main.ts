1a6902f9dbfb19c5fbfc5a3b94b05eb78d972bf4
chaosdynamix <chaosdynamix@posteo.net>
2022-02-17 21:46:19 +0100

    style: change indentation to 4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Page from '@components/page.vue'
+import '@assets/styles/global.css'
 
-createApp(App).mount('#app')
+createApp(Page).mount('#app')
